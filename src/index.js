e678584875c3b47e5df16d8cd2b84d4d078b5c6a
Feng Zhang <7960365+xizero00@users.noreply.github.com>
2024-07-23 12:25:46 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.hitictoc.com": dockerHub,
+  "quay.hitictoc.com": "https://quay.io",
+  "gcr.hitictoc.com": "https://gcr.io",
+  "k8s-gcr.hitictoc.com": "https://k8s.gcr.io",
+  "k8s.hitictoc.com": "https://registry.k8s.io",
+  "ghcr.hitictoc.com": "https://ghcr.io",
+  "cloudsmith.hitictoc.com": "https://docker.cloudsmith.io",
+  "ecr.hitictoc.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.hitictoc.com": dockerHub,
 };
 
 function routeByHosts(host) {
